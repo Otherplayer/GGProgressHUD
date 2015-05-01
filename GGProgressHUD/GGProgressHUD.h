c92ce324fa5b960ae565396b20e4fe75dbe39412
zero <osmk@qq.com>
2015-05-01 23:35:33 +0800

    bulid

---
+++ GGProgressHUD/GGProgressHUD.h
@@ -0,0 +1,13 @@
+//
+//  GGProgressHUD.h
+//  GGProgressHUD
+//
+//  Created by __无邪_ on 15/5/1.
+//  Copyright (c) 2015年 __无邪_. All rights reserved.
+//
+
+#import "MBProgressHUD.h"
+
+@interface GGProgressHUD : MBProgressHUD
+
+@end
---
+++ GGProgressHUD/GGProgressHUD.m
@@ -0,0 +1,21 @@
+//
+//  GGProgressHUD.m
+//  GGProgressHUD
+//
+//  Created by __无邪_ on 15/5/1.
+//  Copyright (c) 2015年 __无邪_. All rights reserved.
+//
+
+#import "GGProgressHUD.h"
+
+@implementation GGProgressHUD
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
