0a8b75308c92b2f1e21b92138f71c222db27065b
zero <osmk@qq.com>
2015-05-01 22:12:09 +0800

    Initial Commit

---
+++ GGProgressHUD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GGProgressHUD
+//
+//  Created by __无邪_ on 15/5/1.
+//  Copyright (c) 2015年 __无邪_. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
